95ba5a35fe4c3ed17c0da27b63d5a17661157b82
gopitha <gopitha@live.no>
2020-08-30 11:49:08 +0200

    Lagt til flere sider og endret litt på tekst

--- FANT2/Controllers/HomeController.cs
+++ FANT2/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace FANT2.Controllers
 			return View();
 		}
 
+        public IActionResult Start()
+        {
+            return View();
+        }
+
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 		public IActionResult Error()
 		{
