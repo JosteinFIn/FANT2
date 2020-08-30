23d8c82ca3f9b685dddc61da68d1554b2dadf5e8
Lema <lemasediqi@gmail.com>
2020-08-30 12:40:21 +0200

    Connected to azure

--- FANT2/Startup.cs
+++ FANT2/Startup.cs
@@ -23,6 +23,8 @@ namespace FANT2
 			Configuration = configuration;
 		}
 
+		//lagt til kommentar for azure
+
 		public IConfiguration Configuration { get; }
 
 		// This method gets called by the runtime. Use this method to add services to the container.
