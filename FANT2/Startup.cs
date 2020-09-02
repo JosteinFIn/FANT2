b93de681dd7e1b5a9fce9618e1e128987911f32e
Jostein Finnekaasa <josteinfin@gmail.com>
2020-09-02 09:40:39 +0200

    Changed connection string i startup

--- FANT2/Startup.cs
+++ FANT2/Startup.cs
@@ -35,7 +35,7 @@ namespace FANT2
 
 			services.AddDbContext<ApplicationDbContext>(options =>
 				options.UseSqlServer(
-					Configuration.GetConnectionString("DefaultConnection")));
+					Configuration.GetConnectionString("MyDbConnection")));
 
 			services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
 				.AddEntityFrameworkStores<ApplicationDbContext>();
